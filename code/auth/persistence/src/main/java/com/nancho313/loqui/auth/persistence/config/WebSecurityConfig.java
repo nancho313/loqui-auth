1de44d830b7cf9274884d491115b686519ecf66a
Hernan Dario Jimenez <nancho313@hotmail.com>
2023-10-20 20:37:10 -0500

    enable cors

--- code/auth/persistence/src/main/java/com/nancho313/loqui/auth/persistence/config/WebSecurityConfig.java
+++ code/auth/persistence/src/main/java/com/nancho313/loqui/auth/persistence/config/WebSecurityConfig.java
@@ -5,27 +5,47 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/**"))
+  
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    
+    http.cors(Customizer.withDefaults())
+            .csrf((csrf) -> csrf.ignoringRequestMatchers("/**"))
             .authorizeHttpRequests(authorize -> authorize.requestMatchers("/signin", "/signup").permitAll());
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder loquiPasswordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
-
+    return http.build();
+  }
+  
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+  
+  @Bean
+  public PasswordEncoder loquiPasswordEncoder() {
+    return new BCryptPasswordEncoder(10);
+  }
+  
 }
